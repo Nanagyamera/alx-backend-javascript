decb0beef4a4fd49f06634a3564b994f769df1a4
Nanagyamera <nanagyamera3@gmail.com>
2023-06-29 08:06:45 -0700

    Implemented HolbertonCourse class with attributes and methods

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = typeof name === 'string' ? name : '';
-    this._length = typeof length === 'number' ? length : 0;
-    this._students = Array.isArray(students) ? students : [];
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
