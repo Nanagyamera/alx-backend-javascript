754f491307fe1afc447353b00abc75c33c6ee6cc
Nanagyamera <nanagyamera3@gmail.com>
2023-06-29 13:11:43 -0700

    Implemented Currency class with attributes and methods

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
