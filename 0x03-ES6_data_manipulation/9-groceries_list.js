a63c7d765240194fa3ab2839fc660b52fd5f5487
Nanagyamera <nanagyamera3@gmail.com>
2023-07-03 16:06:51 -0700

    Added groceriesList function to return a map of groceries with items and quantities

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
   return new Map([
-    ['apples', 5],
-    ['bananas', 3],
-    ['bread', 2],
-    ['milk', 1],
-    ['eggs', 6]
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
   ]);
 }
