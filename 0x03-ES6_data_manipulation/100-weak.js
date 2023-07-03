ca5d713fb51f42af4e22485226584a166b0089cf
Nanagyamera <nanagyamera3@gmail.com>
2023-07-03 16:08:22 -0700

    Added weakMap and queryAPI functions to track and limit the number of queries per endpoint

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,25 +1,13 @@
 export const weakMap = new WeakMap();
 
+const MAX_ENDPOINT_CALLS = 5;
+
 export function queryAPI(endpoint) {
-  const { protocol, name } = endpoint;
-  const key = `${protocol}-${name}`;
-
-  if (!weakMap.has(key)) {
-    weakMap.set(key, 0);
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
   }
-
-  const queryCount = weakMap.get(key);
-  weakMap.set(key, queryCount + 1);
-
-  if (queryCount + 1 >= 5) {
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
     throw new Error('Endpoint load is high');
   }
-
-  // Rest of the logic for querying the API
-  // ...
-
-  // Example return statement
-  return `API query successful for ${protocol}-${name}`;
 }
-
-export { weakMap, queryAPI };
