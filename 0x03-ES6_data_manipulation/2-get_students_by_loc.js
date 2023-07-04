816a1e4d623f041826722ff32ba9ac999ff6183c
Nanagyamera <nanagyamera3@gmail.com>
2023-07-04 02:25:21 -0700

    Added getStudentsByLocation function to filter and return student objects located in a specific city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
 export default function getStudentsByLocation(studentList, city) {
-  if (students instanceof Array) {
-    return studentList.filter(student => student.location === city);
-  }
-  return [];
+  return studentList.filter((student) => student.location === city);
 }
