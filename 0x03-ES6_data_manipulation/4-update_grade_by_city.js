9e58dbf5a23e92daa8e4ba8b959b03b7c9f2a8f8
Nanagyamera <nanagyamera3@gmail.com>
2023-07-05 12:38:58 -0700

    Added updateStudentGradeByCity function to update student grades for a specific city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(studentList, city, newGrades) {
   return studentList
     .filter((student) => student.location === city)
     .map((student) => {
-      const foundGrade = newGrades.find(grade => grade.studentId === student.id);
+      const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
       const grade = foundGrade ? foundGrade.grade : 'N/A';
       return { ...student, grade };
     });
