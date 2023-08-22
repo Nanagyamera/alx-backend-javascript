485f98f3bc623c792d3c00ecbb4123e44e9acff1
Nanagyamera <nanagyamera3@gmail.com>
2023-08-22 08:28:48 -0700

    Recreate HTTP server using Express with additional routes

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,34 +1,94 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async'); // Assuming you have 3-read_file_async.js in the same directory
+const fs = require('fs');
 
 // Create an Express app
 const app = express();
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+const PORT = 1245;
+const databaseFilePath = process.argv[2];
+
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dataPath) {
+    fs.readFile(dataPath, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const reportParts = [];
+        const fileLines = data.toString('utf-8').trim().split('\n');
+        const studentGroups = {};
+        const dbFieldNames = fileLines[0].split(',');
+        const studentPropNames = dbFieldNames.slice(
+          0,
+          dbFieldNames.length - 1,
+        );
+
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(',');
+          const studentPropValues = studentRecord.slice(
+            0,
+            studentRecord.length - 1,
+          );
+          const field = studentRecord[studentRecord.length - 1];
+          if (!Object.keys(studentGroups).includes(field)) {
+            studentGroups[field] = [];
+          }
+          const studentEntries = studentPropNames.map((propName, idx) => [
+            propName,
+            studentPropValues[idx],
+          ]);
+          studentGroups[field].push(Object.fromEntries(studentEntries));
+        }
+
+        const totalStudents = Object.values(studentGroups).reduce(
+          (pre, cur) => (pre || []).length + cur.length,
+        );
+        reportParts.push(`Number of students: ${totalStudents}`);
+        for (const [field, group] of Object.entries(studentGroups)) {
+          reportParts.push([
+            `Number of students in ${field}: ${group.length}.`,
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
+        }
+        resolve(reportParts.join('\n'));
+      }
+    });
+  }
 });
 
-// Define a route for the /students URL
-app.get('/students', (req, res) => {
-  countStudents('database.csv')
-    .then(result => {
-      const response = [
-        'This is the list of our students',
-        `Number of students: ${result.numberOfStudents}`,
-        `Number of students in CS: ${result.firstNameList.CS.length}. List: ${result.firstNameList.CS.join(', ')}`,
-        `Number of students in SWE: ${result.firstNameList.SWE.length}. List: ${result.firstNameList.SWE.join(', ')}`
-      ].join('\n');
-      res.send(response);
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (_, res) => {
+  const responseParts = ['This is the list of our students'];
+
+  countStudents(DB_FILE)
+    .then((report) => {
+      responseParts.push(report);
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
     })
-    .catch(error => {
-      res.status(500).send(error);
+    .catch((err) => {
+      responseParts.push(err instanceof Error ? err.message : err.toString());
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
     });
 });
 
 // Listen on port 1245
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 // Export the app variable
