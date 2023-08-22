106cac064e1db5bb0f91720f9c819cebc2551927
Nanagyamera <nanagyamera3@gmail.com>
2023-08-22 06:40:45 -0700

    Create HTTP server using Express module

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // Define a route for the root URL
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!\n');
 });
 
