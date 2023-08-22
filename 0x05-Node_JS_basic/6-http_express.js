b34c1b15d6e326e060ac44cb0ad8e673091aaa06
Nanagyamera <nanagyamera3@gmail.com>
2023-08-22 06:45:31 -0700

    Create HTTP server using Express module

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,13 +4,13 @@ const express = require('express');
 const app = express();
 
 // Define a route for the root URL
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!\n');
 });
 
 // Listen on port 1245
 app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+  console.log(`Server is listening on port 1245`);
 });
 
 // Export the app variable
